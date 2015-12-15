08b070a72fd3f6e9b1b67099b58776d643d1e549
Alitea Laya Ramos <laya.docusign@gmail.com>
2015-12-14 23:37:52 -0500

    HW 3

---
+++ index.js
@@ -0,0 +1,101 @@
+
+    var express  = require('express');
+    var app      = express();                             
+    var mongoose = require('mongoose');               
+    var morgan = require('morgan');             
+    var bodyParser = require('body-parser');    
+    var methodOverride = require('method-override'); 
+	var mongojs = require('mongojs');
+
+  
+    mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');     // connect to mongoDB database on modulus.io
+	// mongoose.connect('mongodb://localhost/simple');
+	 
+    app.use(express.static(__dirname + '/public'));                 
+    app.use(morgan('dev'));                                         
+    app.use(bodyParser.urlencoded({'extended':'true'}));            
+    app.use(bodyParser.json());                                     
+    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+    app.use(methodOverride());
+
+    app.listen(3000);
+    console.log("App listening on port 3000");
+	
+    var Todo = mongoose.model('Todo', {
+        text : String
+    });
+	
+    app.get('/api/todos', function(req, res) {
+
+        Todo.find(function(err, todos) {
+			if (err)
+                res.send(err)
+
+            res.json(todos); 
+        });
+    });
+	
+   
+    app.post('/api/todos', function(req, res) {
+
+        
+        Todo.create({
+            text : req.body.text,
+            done : false
+        }, function(err, todo) {
+            if (err)
+                res.send(err);
+
+            
+            Todo.find(function(err, todos) {
+                if (err)
+                    res.send(err)
+                res.json(todos);
+            });
+        });
+
+    });
+
+
+    app.delete('/api/todos/:todo_id', function(req, res) {
+        Todo.remove({
+            _id : req.params.todo_id
+        }, function(err, todo) {
+            if (err)
+                res.send(err);
+
+            Todo.find(function(err, todos) {
+                if (err)
+                    res.send(err)
+                res.json(todos);
+            });
+        });
+    });
+
+	app.get('/api/todos/:todo_id', function(req, res) {
+
+        Todo.findOne({
+			_id: req.params.todo_id
+			}, function(err, todos) {
+				if (err)
+                    res.send(err)
+             res.json(todos);
+
+        });
+    });
+	
+	app.put('/api/todos/:todo_id', function(req, res) {
+
+        Todo.findAndModify({
+			query: {_id: req.params.todo_id
+			}, update: {$set: {text: req.body.text}},
+			new: true},
+				function(err, todos) {
+					if (err)
+						res.send(err)
+				 res.json(todos);
+
+        });
+    });
+	
+	
